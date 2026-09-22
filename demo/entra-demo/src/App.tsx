78ef5c809a5d54cca6a188afa3b21c8d4de08448
Patricio <patricio.zetazeta@gmail.com>
2026-09-22 12:24:51 -0300

    primer commit

---
+++ demo/entra-demo/src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <main>
+      <h1>SPA React funcionando</h1>
+      <p>Hito H3: proyecto local preparado.</p>
+    </main>
+  );
+}
+
+export default App
